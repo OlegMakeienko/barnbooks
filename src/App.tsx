6548347e2724f76be37b0d24719949e12be58e76
olegmakeienko <molva83@gmail.com>
2024-05-20 21:39:01 +0200

    useEffect get data from github

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,32 @@
+import {useState, useEffect} from "react";
 import HomePage from "./pages/HomePage.tsx";
 import BookPage from "./pages/BookPage.tsx";
 import {Routes, Route} from "react-router-dom";
+import axios from "axios";
+import BookType from "./models/bookType.ts";
 
 function App() {
+    const [books, setBooks] = useState<BookType[]>([]);
+
+    useEffect(
+        () => {
+            axios.get('https://santosnr6.github.io/Data/childrens_books.json')
+                .then(response =>{
+                    setBooks(response.data);
+                })
+                .catch((error: string) => console.log(error));
+        }, []
+    );
+
+    useEffect(() => {
+        console.log(books)
+    }, [books]);
 
   return (
-    <div className="app">
+      <div className="app">
       <Routes>
-          <Route path="/" element={<HomePage/>}/>
-          <Route path="/book/:id" element={<BookPage/>}/>
+          <Route path="/" element={<HomePage books={books} />} />
+          <Route path="/book/:id" element={<BookPage />} />
       </Routes>
     </div>
   )
